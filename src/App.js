3beed0cb069dd25f881cf9903846a11a1c35b3d5
Anna Lo <annayylo@gmail.com>
2024-01-26 15:53:39 -0500

    portfolio component and layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Portfolio from './Portfolio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Portfolio />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Weeeeeeee...coming soon.
         </p>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/Portfolio.js
@@ -0,0 +1,16 @@
+import './Portfolio.css';
+
+function Portfolio() {
+  return (
+    <div className="Portfolio">
+      <div className="Portfolio-column Portfolio-sidebar">
+        hello
+      </div>
+      <div className="Portfolio-column Portfolio-main">
+        bye
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
