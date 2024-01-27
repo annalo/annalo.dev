e502496baf1e5b7c1557f97834bcab2bfd875901
Anna Lo <annayylo@gmail.com>
2024-01-26 19:15:45 -0500

    about me

---
+++ src/About.js
@@ -0,0 +1,38 @@
+import './About.css';
+
+function About() {
+  return (
+    <div className="About">
+      <p>
+      I'm a full-stack software engineer with expertise in React, GraphQL, APIs, Ruby on Rails, and Elixir. I also have experience in building CMS solutions using platforms like WordPress, Shopify, and Webflow.
+      </p>
+
+      <p>
+      Over the past four years, I've been up to a bunch of stuff, but now I'm ready for some commitment!
+    
+      <ul>
+        <li>Biking/eating through Taipei</li>
+        <li>Scaling Annapurna</li>
+        <li>Roaming Pompeii</li>
+        <li>Studying written Chinese at NTNU</li>
+        <li>Meditating in Jesup</li>
+        <li>Music festivals on 18-wheelers in Poland</li>
+        <li>Hopping islands in Thailand</li>
+        <li>Scooting in Ubud</li>
+        <li>Celebrating in the Big Easy</li>
+        <li>Surfing/floating in Kenting/Phuket/Canggu</li>
+        <li>Locked down in Pokhara</li>
+        <li>Paddling Alaskan glaciers</li>
+        <li>Sak Yant(s) in Lampang</li>
+        <li>Slow-living on Caye Caulker</li>
+        <li>Border-crossing by bus</li>
+        <li>Canyoning in Hualien</li>
+        <li>Getting soaked (Thai New Year) in Chiangmai</li>
+        <li>Busting ribs (punny) in NC</li>
+        </ul>
+      </p>
+    </div>
+  );
+}
+
+export default About;
