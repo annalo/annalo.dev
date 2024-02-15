333415c2a4472b5a3d5121e6172eb9eedf52c4c0
Anna Lo <annayylo@gmail.com>
2024-02-15 17:16:17 -0500

    update about

--- src/About/About.js
+++ src/About/About.js
@@ -1,4 +1,4 @@
-import './About.css';
+  import './About.css';
 
 function About() {
   return (
@@ -8,28 +8,7 @@ function About() {
       </p>
 
       <p>
-      Over the past four years, I've been up to a bunch of stuff, but now I'm ready for some commitment!
-    
-      <ul>
-        <li>Biking/eating through Taipei</li>
-        <li>Scaling Annapurna</li>
-        <li>Roaming Pompeii</li>
-        <li>Studying written Chinese at NTNU</li>
-        <li>Meditating in Jesup</li>
-        <li>Music festivals on 18-wheelers in Poland</li>
-        <li>Hopping islands in Thailand</li>
-        <li>Scooting in Ubud</li>
-        <li>Celebrating in the Big Easy</li>
-        <li>Surfing/floating in Kenting/Phuket/Canggu</li>
-        <li>Locked down in Pokhara</li>
-        <li>Paddling Alaskan glaciers</li>
-        <li>Sak Yant(s) in Lampang</li>
-        <li>Slow-living on Caye Caulker</li>
-        <li>Border-crossing by bus</li>
-        <li>Canyoning in Hualien</li>
-        <li>Getting soaked (Thai New Year) in Chiangmai</li>
-        <li>Busting ribs (punny) in NC</li>
-        </ul>
+      Over the past four years, I've been up to a bunch of stuff, but now I'm ready for some commitment!  
       </p>
     </div>
   );
