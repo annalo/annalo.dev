8d3def093ee8f87c2f046d1ba8b167a834f00a38
Anna Lo <annayylo@gmail.com>
2024-02-16 13:39:29 -0500

    update about and skills

--- src/About/About.js
+++ src/About/About.js
@@ -5,12 +5,18 @@ function About() {
     <div className="About">
       <p>
         👋 I'm a <strong>software engineer</strong> with 8+ years of experience, 
-        specializing in full-stack development with <strong>React</strong>, <strong>GraphQL</strong>, <strong>Ruby on Rails</strong>, 
-        and <strong>Elixir</strong>. I approach software hollistically, considering 
-        all aspects of a project to create <strong>robust</strong> and <strong>effective</strong> solutions.
+        specializing in full-stack development with <strong>JavaScript</strong>, <strong>React</strong>, <strong>GraphQL</strong>, <strong>Ruby on Rails</strong>, 
+        and <strong>Elixir</strong>. Additionally, I'm experienced with CMS products 
+        like <strong>Wordpress</strong> and <strong>Webflow</strong>. I approach 
+        software hollistically, considering all aspects of a project to 
+        create robust and effective solutions.
       </p>
+
+
+
+ My approach to software development is holistic, considering all aspects of a project to create **robust** and **effective** solutions.
       <p>
-        I'm a traveler with a great appreciation for our beautiful blue-green globe. 
+        I'm a traveler with a great appreciation for our beautiful blue-green globe 🌏🏔️🏝️. 
         Over the past few years, I've been up to a bunch of stuff, but now I'm 
         ready for some commitment!
       </p>
