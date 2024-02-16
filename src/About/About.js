9e0433ea84f13e513174ada843e89b9e84aff88d
Anna Lo <annayylo@gmail.com>
2024-02-16 08:18:51 -0500

    formatting

--- src/About/About.js
+++ src/About/About.js
@@ -4,12 +4,12 @@ function About() {
   return (
     <div className="About">
       <p>
-      I'm a full-stack software engineer with expertise in React, GraphQL, APIs, Ruby on Rails, and Elixir. I also have experience in building CMS solutions using platforms like WordPress, Shopify, and Webflow.
+      I'm a full-stack software engineer with expertise in React, GraphQL, APIs, Elixir, and Ruby on Rails. I also have experience in building CMS solutions using platforms like WordPress, Shopify, and Webflow.
       </p>
       <p>
-      Over the past four years, I've been up to a bunch of stuff, but now I'm ready for some commitment!  
+      I'm an avid traveler and a lover of the great outdoors. Over the past few years, I've been up to a bunch of stuff, but now I'm ready for some commitment!  
       </p>
-    </div>
+  </div>
   );
 }
 
--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -5,12 +5,12 @@ import Experience from '../Experience/Experience';
 function Portfolio() {
   return (
     <div className="Portfolio">
-      <div className="Portfolio-column Portfolio-sidebar">
+      <div className="Portfolio-sidebar">
         <div className="Portfolio-name">Anna Lo</div>
         <div className="Portfolio-title">Software Developer</div>
         <About />
       </div>
-      <div className="Portfolio-column Portfolio-main">
+      <div className="Portfolio-main">
         <Experience />
       </div>
     </div>
