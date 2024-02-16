1b781fd2f26c4463b2542e739eb4bf59be27b93d
Anna Lo <annayylo@gmail.com>
2024-02-16 08:53:06 -0500

    update about

--- src/About/About.js
+++ src/About/About.js
@@ -4,10 +4,15 @@ function About() {
   return (
     <div className="About">
       <p>
-      I'm a full-stack software engineer with expertise in React, GraphQL, APIs, Elixir, and Ruby on Rails. I also have experience in building CMS solutions using platforms like WordPress, Shopify, and Webflow.
+        👋 I'm a <strong>software engineer</strong> with 8+ years of experience, 
+        specializing in full-stack development with <strong>React</strong>, <strong>GraphQL</strong>, <strong>Ruby on Rails</strong>, 
+        and <strong>Elixir</strong>. I approach software hollistically, considering 
+        all aspects of a project to create <strong>robust</strong> and <strong>effective</strong> solutions.
       </p>
       <p>
-      I'm an avid traveler and a lover of the great outdoors. Over the past few years, I've been up to a bunch of stuff, but now I'm ready for some commitment!  
+        I'm a traveler with a great appreciation for our beautiful blue-green globe. 
+        Over the past few years, I've been up to a bunch of stuff, but now I'm 
+        ready for some commitment!
       </p>
   </div>
   );
