177625be6a2142c720af0b1b36745641ca98b670
Anna Lo <annayylo@gmail.com>
2024-02-17 16:09:56 -0500

    projects section

---
+++ src/Projects/Projects.js
@@ -0,0 +1,14 @@
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className="Projects">
+      <h4>Projects</h4>
+      <p>
+        work work work
+      </p>
+  </div>
+  );
+}
+
+export default Projects;
