ff8990b1ecf2ea1632ef57d0754c53b60a53b896
Anna Lo <annayylo@gmail.com>
2024-02-18 17:09:33 -0500

    refactor portolio media queries

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,6 +1,6 @@
 import './Portfolio.css';
 
-import Sidebar from '../Sidebar/Sidebar';
+import SidebarMenu from '../Sidebar/SidebarMenu'
 import About from '../About/About';
 import Experience from '../Experience/Experience';
 import Projects from '../Projects/Projects'
@@ -8,9 +8,13 @@ import Projects from '../Projects/Projects'
 function Portfolio() {
   return (
     <div className="Portfolio">
-      <Sidebar />
+      <div className='Portfolio-sidebar'>
+        <div className="Portfolio-sidebar-name">Anna Lo</div>
+        <div className="Portfolio-sidebar-title">Software Engineer</div>
+        <SidebarMenu />
+      </div>
       
-      <div className="Portfolio-container">
+      <div className="Portfolio-main">
         <About />
         <Experience />
         <Projects />
