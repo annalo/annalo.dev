e5added4f858ee0708c804f08315fd1f87f12bde
Anna Lo <annayylo@gmail.com>
2024-02-17 19:39:38 -0500

    meh

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,13 +1,11 @@
 import './Sidebar.css';
-import SidebarMenu from './SidebarMenu';
+// import SidebarMenu from './SidebarMenu';
 
 function Sidebar () {
   return (
     <div className="Sidebar">
       <div className="Sidebar-name">Anna Lo</div>
       <div className="Sidebar-title">Software Engineer</div>
-
-      <SidebarMenu />
     </div>
   )
 }
