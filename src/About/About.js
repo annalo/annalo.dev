87862c5ecc271ea0dc3728c02e0b87996860477f
Anna Lo <annayylo@gmail.com>
2024-02-19 02:58:02 -0500

    text-wrap balance

--- src/About/About.js
+++ src/About/About.js
@@ -5,20 +5,17 @@ function About() {
     <div className="About">
       <h4>About</h4>
       <p>
-        👋 I'm a <strong>software engineer</strong> with 8+ years of experience, 
-        specializing in full-stack development with <strong>JavaScript</strong>, <strong>React</strong>, <strong>GraphQL</strong>, <strong>Ruby on Rails</strong>, 
-        and <strong>Elixir</strong>. Additionally, I'm skilled in CMS products 
-        like <strong>Wordpress</strong> and <strong>Webflow</strong>. 
+        👋 I'm a <strong>software engineer</strong> with 8+ years of experience, specializing in full-stack development 
+        with <strong>JavaScript</strong>, <strong>React</strong>, <strong>GraphQL</strong>, <strong>Ruby on Rails</strong>, and <strong>Elixir</strong>. 
+        Additionally, I'm skilled in CMS products like <strong>Wordpress</strong> and <strong>Webflow</strong>. 
       </p>
       <p>
-        I approach software <strong>holistically</strong>, considering all 
-        aspects of a project to create robust and effective solutions.
+        I approach software <strong>holistically</strong>, considering 
+        all aspects of a project to create robust and effective solutions.
       </p>
       <p>
-        I'm an avid traveler with a deep appreciation 
-        for our beautiful blue-green globe 🌏🏔️🏝️. 
-        Over the past few years, I've explored various adventures, 
-        but now I'm ready to commit to new challenges!
+        I'm an avid traveler with a deep appreciation for our beautiful blue-green globe 🌏🏔️🏝️. Over the 
+        past few years, I've explored various adventures, but now I'm ready to commit to new challenges!
       </p>
   </div>
   );
