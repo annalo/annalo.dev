4114295f6e59e4c403586596e2b145c8f835157c
Anna Lo <annayylo@gmail.com>
2024-02-21 11:22:06 -0500

    h5 to h3

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,21 +2,21 @@ import Section from './Section';
 import data from "./Projects.json";
 import ProjectsItem from "./ProjectsItem";
 
-function Projects() {
+function Projects() { 
   return (
     <Section id='projects'>
       <h2>Projects</h2>
 
-      <h5>Fullstack</h5>      
+      <h3>Fullstack</h3>      
       {data.backend.map((item, idx) => <ProjectsItem key={`backend-${idx}`} {...item} />)}
 
-      <h5>Frontend</h5>
+      <h3>Frontend</h3>
       {data.frontend.map((item, idx) => <ProjectsItem key={`frontend-${idx}`} {...item} />)}
 
-      <h5>Backend</h5>
+      <h3>Backend</h3>
       {data.frontend.map((item, idx) => <ProjectsItem key={`fullstack-${idx}`} {...item} />)}
 
-      <h5>CMS</h5>
+      <h3>CMS</h3>
       {data.cms.map((item, idx) => <ProjectsItem key={`cms-${idx}`} {...item} />)}
   </Section>
   )
