cbfee12edd8e4d2658b40bba19a5ef3809a74f6d
Anna Lo <annayylo@gmail.com>
2024-02-21 20:39:08 -0500

    rel="noreferrer" warning

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -7,16 +7,16 @@ import instagramIcon from '../assets/instagram.png';
 function Contacts({skill}) {
   return (
     <div className="Contacts">
-      <a href='https://www.linkedin.com/in/aylo/' target='_blank'>
+      <a href='https://www.linkedin.com/in/aylo/' rel="noreferrer" target='_blank'>
         <img src={linkedInIcon} alt='linkedin' />
       </a>
-      <a href='https://github.com/annalo/' target='_blank'>
+      <a href='https://github.com/annalo/' rel="noreferrer" target='_blank'>
         <img src={githubIcon} alt='github' />
       </a>
-      <a href='mailto:annayylo@gmail.com' target='_blank'>
+      <a href='mailto:annayylo@gmail.com' rel="noreferrer" target='_blank'>
         <img src={emailIcon} alt='email' />
       </a>
-      <a href='https://www.instagram.com/grrroar/' target='_blank'>
+      <a href='https://www.instagram.com/grrroar/' rel="noreferrer" target='_blank'>
         <img src={instagramIcon} alt='instagram' />
       </a>
     </div>
